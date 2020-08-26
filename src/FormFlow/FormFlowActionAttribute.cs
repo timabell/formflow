fccb8f09c9bd4854899d11346dd1d5b52f191a11
James Gunn <james@gunn.io>
2020-08-26 14:59:59 +0100

    Add controller/action attribute for annotating actions with flow metadata

---
+++ src/FormFlow/FormFlowActionAttribute.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using FormFlow.Metadata;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace FormFlow
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false)]
+    public sealed class FormFlowActionAttribute : Attribute, IActionModelConvention, IControllerModelConvention
+    {
+        public FormFlowActionAttribute(string key, Type stateType)
+        {
+            Key = key ?? throw new ArgumentNullException(nameof(key));
+            StateType = stateType ?? throw new ArgumentNullException(nameof(stateType));
+        }
+
+        public string Key { get; }
+
+        public Type StateType { get; }
+
+        void IActionModelConvention.Apply(ActionModel action)
+        {
+            AddMetadataToAction(action);
+        }
+
+        void IControllerModelConvention.Apply(ControllerModel controller)
+        {
+            foreach (var action in controller.Actions)
+            {
+                AddMetadataToAction(action);
+            }
+        }
+
+        private void AddMetadataToAction(ActionModel action)
+        {
+            var descriptor = new FormFlowActionDescriptor(Key, StateType);
+
+            action.Properties.Add(typeof(FormFlowActionDescriptor), descriptor);
+        }
+    }
+}
