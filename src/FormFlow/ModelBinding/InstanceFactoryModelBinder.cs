39f952c8ee1fa2d05fdbb827dd16c0f32c7e1a89
James Gunn <james@gunn.io>
2020-08-26 16:25:41 +0100

    Add a model binder for InstanceFactory

---
+++ src/FormFlow/ModelBinding/InstanceFactoryModelBinder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using FormFlow.Metadata;
+using FormFlow.State;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace FormFlow.ModelBinding
+{
+    public class InstanceFactoryModelBinder : IModelBinder
+    {
+        private readonly IInstanceStateProvider _stateProvider;
+
+        public InstanceFactoryModelBinder(IInstanceStateProvider stateProvider)
+        {
+            _stateProvider = stateProvider ?? throw new ArgumentNullException(nameof(stateProvider));
+        }
+
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            if (bindingContext.ModelType != typeof(InstanceFactory))
+            {
+                return Task.CompletedTask;
+            }
+
+            var flowDescriptor = bindingContext.ActionContext.ActionDescriptor.GetProperty<FormFlowActionDescriptor>();
+            if (flowDescriptor == null)
+            {
+                return Task.CompletedTask;
+            }
+
+            var instanceFactory = new InstanceFactory(flowDescriptor, bindingContext.ActionContext, _stateProvider);
+            bindingContext.Result = ModelBindingResult.Success(instanceFactory);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/FormFlow/ModelBinding/InstanceFactoryModelBinderProvider.cs
@@ -0,0 +1,24 @@
+﻿using FormFlow.State;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FormFlow.ModelBinding
+{
+    public class InstanceFactoryModelBinderProvider : IModelBinderProvider
+    {
+        public InstanceFactoryModelBinderProvider()
+        {
+        }
+
+        public IModelBinder GetBinder(ModelBinderProviderContext context)
+        {
+            if (context.Metadata.ModelType == typeof(InstanceFactory))
+            {
+                var stateProvider = context.Services.GetRequiredService<IInstanceStateProvider>();
+                return new InstanceFactoryModelBinder(stateProvider);
+            }
+
+            return null;
+        }
+    }
+}
