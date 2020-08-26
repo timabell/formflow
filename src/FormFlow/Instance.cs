fe264caba7be7df190d17dae59ca782130cde9e8
James Gunn <james@gunn.io>
2020-08-26 15:28:24 +0100

    Add generic Instance<T> and use this everywhere

--- src/FormFlow/Instance.cs
+++ src/FormFlow/Instance.cs
@@ -5,7 +5,7 @@ namespace FormFlow
 {
     public class Instance
     {
-        public Instance(
+        private protected Instance(
             string key,
             string instanceId,
             Type stateType,
@@ -28,5 +28,30 @@ namespace FormFlow
         public object State { get; }
 
         public Type StateType { get; set; }
+
+        public static Instance Create(
+            string key,
+            string instanceId,
+            Type stateType,
+            object state,
+            IReadOnlyDictionary<string, object> items)
+        {
+            var genericType = typeof(Instance<>).MakeGenericType(stateType);
+            return (Instance)Activator.CreateInstance(genericType, key, instanceId, state, items);
+        }
+    }
+
+    public sealed class Instance<TState> : Instance
+    {
+        public Instance(
+            string key,
+            string instanceId,
+            TState state,
+            IReadOnlyDictionary<string, object> items)
+            : base(key, instanceId, typeof(TState), state, items)
+        {
+        }
+
+        public new object State => (TState)base.State;
     }
 }
