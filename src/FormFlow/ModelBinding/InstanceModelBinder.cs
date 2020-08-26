31bafb757ae93c4bdcfde9029a5b3d5d0c4ec8ce
James Gunn <james@gunn.io>
2020-08-26 17:29:28 +0100

    Add model binder for Instance

---
+++ src/FormFlow/ModelBinding/InstanceModelBinder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using FormFlow.Metadata;
+using FormFlow.State;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace FormFlow.ModelBinding
+{
+    public class InstanceModelBinder : IModelBinder
+    {
+        private readonly IInstanceStateProvider _stateProvider;
+        private readonly IdResolver _idResolver = new IdResolver();
+
+        public InstanceModelBinder(IInstanceStateProvider stateProvider)
+        {
+            _stateProvider = stateProvider ?? throw new ArgumentNullException(nameof(stateProvider));
+        }
+
+        public async Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            if (bindingContext.ModelType != typeof(Instance) &&
+                !(bindingContext.ModelType.IsGenericType && bindingContext.ModelType.GetGenericTypeDefinition() == typeof(Instance<>)))
+            {
+                return;
+            }
+
+            var flowDescriptor = bindingContext.ActionContext.ActionDescriptor.GetProperty<FormFlowActionDescriptor>();
+            if (flowDescriptor == null)
+            {
+                return;
+            }
+
+            var instanceId = _idResolver.ResolveId(bindingContext.ActionContext, flowDescriptor);
+            if (string.IsNullOrEmpty(instanceId))
+            {
+                return;
+            }
+
+            var instance = await _stateProvider.GetInstance(instanceId);
+            if (instance == null)
+            {
+                return;
+            }
+
+            bindingContext.Result = ModelBindingResult.Success(instance);
+        }
+    }
+}
---
+++ src/FormFlow/ModelBinding/InstanceModelBinderProvider.cs
@@ -0,0 +1,21 @@
+﻿using FormFlow.State;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FormFlow.ModelBinding
+{
+    public class InstanceModelBinderProvider : IModelBinderProvider
+    {
+        public IModelBinder GetBinder(ModelBinderProviderContext context)
+        {
+            if (context.Metadata.ModelType == typeof(Instance) ||
+                (context.Metadata.ModelType.IsGenericType && context.Metadata.ModelType.GetGenericTypeDefinition() == typeof(Instance<>)))
+            {
+                var stateProvider = context.Services.GetRequiredService<IInstanceStateProvider>();
+                return new InstanceModelBinder(stateProvider);
+            }
+
+            return null;
+        }
+    }
+}
