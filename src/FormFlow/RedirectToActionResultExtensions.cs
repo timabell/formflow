9367c0b914f6a3d3f5add1b0ee0197f23ab4053c
James Gunn <james@gunn.io>
2020-08-27 14:49:20 +0100

    Add extension methods for redirecting to an action with route values for the given instance

---
+++ src/FormFlow/RedirectToActionResultExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace FormFlow
+{
+    public static class RedirectToActionResultExtensions
+    {
+        public static RedirectToActionResult WithFormFlowInstanceId(
+            this RedirectToActionResult result,
+            Instance instance)
+        {
+            return WithFormFlowInstanceId(result, instance.InstanceId);
+        }
+
+        public static RedirectToActionResult WithFormFlowInstanceId(
+            this RedirectToActionResult result,
+            InstanceId instanceId)
+        {
+            result.RouteValues ??= new RouteValueDictionary();
+
+            foreach (var kvp in instanceId.RouteValues)
+            {
+                result.RouteValues.Add(kvp.Key, kvp.Value);
+            }
+
+            return result;
+        }
+    }
+}
