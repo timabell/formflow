7d96bcde82d9194c0172b47c32b3c877573b6f2a
James Gunn <james@gunn.io>
2020-08-27 15:41:56 +0100

    Add InstanceProvider - a mechanism for services to resolve the current instance through DI

---
+++ src/FormFlow/InstanceProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+
+namespace FormFlow
+{
+    public class InstanceProvider
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public InstanceProvider(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor ?? throw new ArgumentNullException(nameof(httpContextAccessor));
+        }
+
+        public Instance GetInstance()
+        {
+            var httpContext = _httpContextAccessor.HttpContext;
+            return httpContext.Features.Get<FormFlowInstanceFeature>()?.Instance;
+        }
+
+        public Instance<T> GetInstance<T>()
+        {
+            return (Instance<T>)GetInstance();
+        }
+    }
+}
---
+++ src/FormFlow/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FormFlow
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddFormFlow(this IServiceCollection services)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            services.AddHttpContextAccessor();
+            services.AddSingleton<InstanceProvider>();
+
+            return services;
+        }
+    }
+}
