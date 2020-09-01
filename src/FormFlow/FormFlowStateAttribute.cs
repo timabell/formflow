4af4dbc11827a466169f84bc3873e779af1ad930
James Gunn <james@gunn.io>
2020-09-01 09:40:15 +0100

    Add an extension method for registering FormFlowInstance<T>s
    Any type in the specified assembly annotated with FormFlowStateAttribute
    will have a registration added.

---
+++ src/FormFlow/FormFlowStateAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FormFlow
+{
+    /// <summary>
+    /// Marker attribute for annotating FormFlow state types.
+    /// </summary>
+    /// <remarks>
+    /// Used by <see cref="ServiceCollectionExtensions.AddFormFlowStateTypes(Microsoft.Extensions.DependencyInjection.IServiceCollection, System.Reflection.Assembly)"/>
+    /// for identifying the state types to register strongly-typed instances for in the DI container.
+    /// This enables resolving <see cref="FormFlowInstance{TState}"/> when <c>TState</c> has been decorated with this attribute.
+    /// </remarks>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public sealed class FormFlowStateAttribute : Attribute
+    {
+    }
+}
--- src/FormFlow/ServiceCollectionExtensions.cs
+++ src/FormFlow/ServiceCollectionExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection;
 using FormFlow.Filters;
 using FormFlow.ModelBinding;
 using FormFlow.State;
@@ -51,6 +53,54 @@ namespace FormFlow
             services.AddFormFlow();
 
             return services;
+        }
+
+        public static IServiceCollection AddFormFlowStateTypes(
+            this IServiceCollection services,
+            Assembly assembly)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            if (assembly == null)
+            {
+                throw new ArgumentNullException(nameof(assembly));
+            }
+
+            var stateTypes = assembly.GetTypes()
+                .Where(t => t.IsPublic && !t.IsAbstract && t.GetCustomAttribute<FormFlowStateAttribute>() != null);
+
+            foreach (var type in stateTypes)
+            {
+                var instanceType = typeof(FormFlowInstance<>).MakeGenericType(type);
+
+                services.AddTransient(instanceType, sp =>
+                {
+                    var instanceProvider = sp.GetRequiredService<FormFlowInstanceProvider>();
+                    return instanceProvider.GetInstance();
+                });
+            }
+
+            return services;
+        }
+
+        public static IServiceCollection AddFormFlowStateTypes(
+            this IServiceCollection services,
+            Type fromAssemblyContainingType)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            if (fromAssemblyContainingType == null)
+            {
+                throw new ArgumentNullException(nameof(fromAssemblyContainingType));
+            }
+
+            return AddFormFlowStateTypes(services, fromAssemblyContainingType.Assembly);
         }
     }
 }
